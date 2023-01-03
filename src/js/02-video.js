326feab393333dcabdb9fa6a0e94c081b49a321f
Daniil <x6uhrox@gmail.com>
2023-01-03 07:33:41 +0200

    Fix exercise №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,19 @@ const player = new Player("vimeo-player");
 const throttle = require("lodash.throttle");
 
 player.on("timeupdate", throttle(onSaveTimePlay, 1000));
+playSaveTime();
 
 function onSaveTimePlay(data) {
   const currentTimePlay = data.seconds;
   console.log(currentTimePlay);
   localStorage.setItem("videoplayer-current-time", currentTimePlay);
 }
-const saveCurrentTime = localStorage.getItem("videoplayer-current-time");
 
-player.setCurrentTime(saveCurrentTime);
+function playSaveTime() {
+  const saveCurrentTime = localStorage.getItem("videoplayer-current-time");
+  if (saveCurrentTime) {
+  player.setCurrentTime(saveCurrentTime);
+  }
+}
+
+
